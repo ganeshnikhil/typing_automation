14cbefadbe9968464f7ac31baf4eb900f121da56
ganesh nikhil <73976037+ganeshnikhil@users.noreply.github.com>
2023-10-14 08:25:03 +0530

    Update sc.py

--- sc.py
+++ sc.py
@@ -4,15 +4,15 @@ import keyboard
 import pytesseract
 from PIL import Image
 
-
-def write_using_keyboard(text):
-  
+# write text using key press on screen
+def write_using_keyboard(text:str) -> None:
   for char in text.strip():
     keyboard.press(char)
     keyboard.release(char)
     time.sleep(0.06)
 
-def extract_text_from_image(filepath):
+# Extract text from the image 
+def extract_text_from_image(filepath:str) -> str:
   # Read the image
   img = Image.open(filepath)
   # Convert the image to grayscale
@@ -21,37 +21,43 @@ def extract_text_from_image(filepath):
   thresh = gray.point(lambda x: 255 if x > 127 else 0)
   # Recognize the text in the image
   text = pytesseract.image_to_string(thresh)
-  # Print the text
+  
   return text
 
 
-
-def take_screenshot(width, height):
+# capture the image of text on screen , pass the precise parameters for result
+def take_screenshot(width:int , height:int  , top:int  , left:int) -> str:
   with mss.mss() as sct:
     # The screen part to capture
-      monitor = {"top": 320, "left": 250, "width": width, "height": height}
-      output = "sct-{top}x{left}_{width}x{height}.png".format(**monitor)
-      #time.sleep(5)
+      monitor = {"top": top , "left": left , "width": width, "height": height}
+      output = f"sct-{top}x{left}_{width}x{height}.png"
     # Grab the data
       sct_img = sct.grab(monitor)
 
     # Save to the picture file
       mss.tools.to_png(sct_img.rgb, sct_img.size, output=output)
   return output
-# Specify the desired width and height
+
+
 def main():
+  # adjust the desired width and height 
   desired_width =1000
   desired_height = 180
+  top = 320
+  left = 250
+  
   print("Press Enter to take a screenshot. Press 'q' to exit.")
   while True:
+    # esc take screenshot of screen 
       if keyboard.is_pressed('esc'):
-          image_name=take_screenshot(desired_width,desired_height)
+          image_name=take_screenshot(desired_width , desired_height , top , left)
           text=extract_text_from_image(image_name)
           if text== None or len(text)<10:
             pass
           else:
             write_using_keyboard(text)
             #print(text)
+     # q quit the program 
       elif keyboard.is_pressed('q'):
           print("Exiting...")
           break
